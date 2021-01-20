f2f7f4b579ed50841ae2461875ce2b245c346214
walker <walker@wishnt.com>
2021-01-20 18:31:41 +0800

    01-20 office学习

---
+++ src/code.walker/day05/02struct/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+//定义结构体
+type person struct {
+	name string
+	age int
+	city string
+}
+type person1 struct {
+	name,city string
+	age int
+}
+func main() {
+	var p1 person
+	p1.name = "沙河娜扎"
+	p1.city = "北京"
+	p1.age = 18
+	fmt.Printf("p1=%v\n", p1)  //p1={沙河娜扎 北京 18}
+	fmt.Printf("p1=%#v\n", p1) //p1=main.person{name:"沙河娜扎", city:"北京", age:18}
+	fmt.Println(p1.name)
+}
