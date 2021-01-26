064d9a0ff78c8900a1abce1a4c00efad8fe96499
walker <walker@wishnt.com>
2021-01-26 16:31:51 +0800

    01-27 office 学习

--- src/code.walker/day06/08struct_work/main.go
+++ src/code.walker/day06/08struct_work/main.go
@@ -5,11 +5,37 @@ import (
 	"os"
 )
 
+func getInput() *student {
+	var (
+		id int
+		name string
+		class string
+	)
+	fmt.Println("请按要求输入学员信息")
+	fmt.Print("请输入学员的学号：")
+	fmt.Scanf("%d\n",&id)
+	fmt.Print("请输入学员的姓名：")
+	fmt.Scanf("%s\n",&name)
+	fmt.Print("请输入学员的班级：")
+	fmt.Scanf("%s\n",&class)
+	// 拿到输入的信息
+	stu := newStudent(id,name,class)//调用student 的构造函数构造一个学生
+	return stu
+}
+
+func getId()int{
+	var id int
+	fmt.Print("请输入学员的学号：")
+	fmt.Scanf("%d\n",&id)
+	return id
+}
+
 // 学员信息管理系统 需求
 //1.添加学员信息
 //2.编辑学员信息
 //3.展示所有学员信息
 func main() {
+	sm := newStudentMagr()
 	for  {
 		// 1.打印系统菜单
 		showMenu()
@@ -22,15 +48,21 @@ func main() {
 		switch input {
 		case 1:
 			// 添加学员
+			stu := getInput()
+			sm.addStudent(stu)
 		case 2:
 			// 编辑学员
+			stu := getInput()
+			sm.editStudent(stu)
 		case 3:
 			// 展示
+			sm.showStudent()
 		case 4:
+			id := getId()
+			sm.delStudent(id)
+		case 5:
 			//退出
 			os.Exit(0)
-
-
 		}
 	}
 
@@ -43,6 +75,7 @@ func showMenu()  {
 	fmt.Println("1.添加学员")
 	fmt.Println("2.编辑学员")
 	fmt.Println("3.展示学员")
-	fmt.Println("4.退出系统")
+	fmt.Println("4.删除学员")
+	fmt.Println("5.退出系统")
 }
 
--- src/code.walker/day06/08struct_work/student.go
+++ src/code.walker/day06/08struct_work/student.go
@@ -1,12 +1,11 @@
 package main
+
+import "fmt"
+
 type student struct {
 	id int //学号是唯一的
 	name string
 	class string
-}
-
-type studentMgr struct {
-	allStudent []*student
 }
 
 // student 类型的构造函数
@@ -17,6 +16,61 @@ func newStudent(id int,name string,class string) *student{
 		class: class,
 	}
 }
-func main() {
 
+type studentMgr struct {
+	allStudents []*student
+}
+
+// newStudentMagr 是studentMgr 的构造函数
+func newStudentMagr() *studentMgr {
+	return &studentMgr{
+		allStudents: make([]*student,0,100),
+	}
+}
+
+// 1.添加学生
+func (s *studentMgr)addStudent(newStu *student)  {
+	exist := true
+	for _,stu := range s.allStudents{
+		if newStu.id == stu.id {
+			fmt.Printf("学号已经存在，请重新输入\n")
+			exist = false
+			break
+		}
+	}
+	if exist{
+		s.allStudents = append(s.allStudents,newStu)
+		fmt.Print("添加成功\n")
+	}
+}
+// 2.编辑学生
+func (s *studentMgr)editStudent(newStu *student)  {
+	for key,student := range s.allStudents{
+		if newStu.id == student.id { // 学号相同
+			s.allStudents[key] = newStu //根据切片的索引直接吧新学生赋值
+			fmt.Print("编辑成功\n")
+			return
+		}
+	}
+	fmt.Printf("输入的学生信息有误，系统中没有学号是：%d的学生\n",newStu.id)
+}
+// 3.展示学生
+func (s *studentMgr)showStudent()  {
+	for _,student := range s.allStudents{
+		fmt.Printf("学号：%d 姓名：%s 班级：%s\n",student.id,student.name,student.class)
+	}
+}
+
+// 4.删除学生
+func (s *studentMgr)delStudent(id int)  {
+	newStudents := make([]*student,0,200)
+	for k,student := range s.allStudents{
+		if id == student.id {
+			fmt.Printf("del stu id: %#v\n", id)
+			continue
+		}
+		newStudents = append(newStudents, s.allStudents[k])
+		s.allStudents = newStudents
+	}
+	fmt.Println("删除成功！！")
 }
